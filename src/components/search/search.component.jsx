1abbfaaa1e096a525f171a10ba1dec2f6ef38111
yRokach <yehuda@slidely.com>
2021-03-24 20:41:05 +0200

    fix: bug with show more

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -10,52 +10,61 @@ function Search() {
     const [pages, setPages] = useState(null);
     const [query, setQuery] = useState('');
     const [currentPage, setCurrentPage] = useState(0);
-    const [isMore, setIsMore] = useState(false);
+    const [hasMore, setHasMore] = useState(false);
     const [isEmptyQuery, setIsEmptyQuery] = useState(true);
     const [searchError, setSearchError] = useState(false);
     const calcPages = (totalResults) => {
         return Math.ceil(totalResults / 10);
     }
-    const doSearch = async (page, isNewSearch) => {
-        if (!isNewSearch && pages !== null && pages <= page) {
-            setIsMore(false);
-            return;
-        }
+    const initSearchState = () => {
         setSearchError(false);
         setIsEmptyQuery(false);
+    }
+    const emptySearch = () => {
+        setIsEmptyQuery(true);
+        setMovies([]);
+    }
+    const doSearch = async (page, isNewSearch) => {
+        if (!isNewSearch && pages !== null && pages <= page) {
+            setHasMore(false);
+            return;
+        }
+        initSearchState();
         if (query === '') {
-            setIsEmptyQuery(true);
-            setMovies([]);
+            emptySearch();
             return;
         }
         const res = await searchService.searchMovieByName(query, page);
         if (Math.floor(res.status / 100) !== 2) {
             setMovies([]);
+            setHasMore(false);
             throw new Error();
         }
         const {data} = res;
-        if (data.Search) {
-            setCurrentPage(page);
-            if (page === 1) {
-                if (data.totalResults) {
-                    setPages(calcPages(data.totalResults));
-                }
-                setMovies(data.Search);
-            } else {
-                setMovies([...movies, ...data.Search]);
-            }
-            if (calcPages(data.totalResults) > page) {
-                setIsMore(true);
-            }
-        }
         if (data.Error) {
             setMovies([]);
-            setSearchError(data.Error)
+            setSearchError(data.Error);
+            return;
         }
+
+        setCurrentPage(page);
+        if (page === 1) {
+            if (data.totalResults) {
+                setPages(calcPages(data.totalResults));
+            }
+            setMovies(data.Search);
+        } else {
+            setMovies([...movies, ...data.Search]);
+        }
+        if (calcPages(data.totalResults) > page) {
+            setHasMore(true);
+            return;
+        }
+        setHasMore(false);
     }
     return (
         <>
-            <SearchContext.Provider value={{doSearch, movies, searchError, isEmptyQuery, isMore, currentPage, query, setQuery}}>
+            <SearchContext.Provider value={{doSearch, movies, searchError, isEmptyQuery, isMore: hasMore, currentPage, query, setQuery}}>
                 <header className="header">
                     <SearchBar />
                 </header>
